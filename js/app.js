a98ff4bd55c52582dac8f31fe948044df9d477b6
Steven Ariol <stevenariol@Stevens-MacBook-Air.local>
2018-09-13 16:44:15 -0600

    Commit 1 - Verbal questions

---
+++ js/app.js
@@ -0,0 +1,18 @@
+// console.log("JS is up");
+
+// 1. Verbal questions
+
+/* A parameter is a variable in a method definition.
+An argument is the actual value of this variable that gets
+passed to the function. */
+
+/* Console.log prints the output in the console. Return
+will return a value when the function is called */
+
+/* The scope of a variable is based on the location
+of the variable declaration. Meaning that if you define
+a variable inside of a function, you will be able to 
+call it back only in the confines of that function. 
+However, if you define a variable outside of a function, 
+you can always call it back because it is not confined to the 
+function. */
